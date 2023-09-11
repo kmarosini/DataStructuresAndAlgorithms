3da864a51e174ae3fb045fb0934d1155131125e6
Konichwa1998 <karlomarosini14@gmail.com>
2023-09-11 20:22:06 +0200

    Linked List methods implemented.

--- src/main/java/hr/dsalgo/Main.java
+++ src/main/java/hr/dsalgo/Main.java
@@ -7,9 +7,8 @@ public class Main {
 
         LinkedList linkedList = new LinkedList(4);
         linkedList.append(2);
-        linkedList.printHead();
-        linkedList.prepend(6);
-        linkedList.printHead();
+        linkedList.removeFirst();
+        linkedList.printList();
 
     }
 }
--- src/main/java/hr/dsalgo/linkedlist/LinkedList.java
+++ src/main/java/hr/dsalgo/linkedlist/LinkedList.java
@@ -1,5 +1,7 @@
 package hr.dsalgo.linkedlist;
 
+import java.util.IllegalFormatCodePointException;
+
 public class LinkedList {
     private Node head;
     private Node tail;
@@ -38,6 +40,7 @@ public class LinkedList {
     // O(1)
     public void append(int value) {
         Node newNode = new Node(value);
+
         if (length == 0) {
             head = newNode;
             tail = newNode;
@@ -61,11 +64,9 @@ public class LinkedList {
             pre = temp;
             temp = temp.next;
         }
-
         tail = pre;
         tail.next = null;
         length--;
-
         if (length == 0) {
             head = null;
             tail = null;
@@ -75,6 +76,7 @@ public class LinkedList {
 
     public void prepend(int value) {
         Node newNode = new Node(value);
+
         if (length == 0) {
             head = newNode;
             tail = newNode;
@@ -83,5 +85,102 @@ public class LinkedList {
             head = newNode;
         }
         length++;
+    }
+
+    public Node removeFirst() {
+        if (length == 0) {
+            return null;
+        }
+
+        Node temp = head;
+
+        head = head.next;
+        temp.next = null;
+        length--;
+        if (length == 0) {
+            tail = null;
+        }
+        return temp;
+    }
+
+    public Node get(int index) {
+        if (index < 0 || index >= length ) {
+            return null;
+        }
+
+        Node temp = head;
+
+        for (int i = 0; i < index; i++) {
+            temp = temp.next;
+        }
+        return temp;
+    }
+
+    public boolean set(int index, int value) {
+        Node temp = get(index);
+
+        if(temp != null) {
+            temp.value = value;
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public boolean insert(int index, int value) {
+        if (index < 0 || index > length) {
+            return false;
+        }
+        if (index == 0) {
+            prepend(value);
+            return true;
+        } else if(index == length) {
+            append(value);
+            return true;
+        }
+
+        Node newNode = new Node(value);
+        Node temp = get(index - 1);
+
+        newNode.next = temp.next;
+        temp.next = newNode;
+        length++;
+        return true;
+    }
+
+    public Node remove(int index) {
+        if (index < 0 || index >= length) {
+            return null;
+        }
+        if (index == 0) {
+            return removeFirst();
+        }
+        if(index == length - 1) {
+            return removeLast();
+        }
+
+        Node prev = get(index-1);
+        Node temp = prev.next;
+
+        prev.next = temp.next;
+        temp.next = null;
+        length--;
+        return temp;
+    }
+
+    public void reverse() {
+        Node temp = head;
+        head = tail;
+        tail = temp;
+
+        Node after = temp.next;
+        Node before = null;
+
+        for (int i = 0; i < length; i++) {
+            after = temp.next;
+            temp.next = before;
+            before = temp;
+            temp = after;
+        }
     }
 }
